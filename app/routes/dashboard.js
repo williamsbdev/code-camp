479a5f55cc8227f3aae2b90bb3ee7c112beb949f
Brandon Williams <williamsbdev@gmail.com>
2015-06-23 19:31:12 -0500

    [FEATURE] dashboard route with data

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model: function() {
+        return [
+            {
+                id: 1,
+                speaker: "Brandon Williams",
+                session: "Ember CLI"
+            },
+            {
+                id: 2,
+                speaker: "Ryan Doll",
+                session: "Intro to Ember"
+            }
+        ];
+    }
 });
